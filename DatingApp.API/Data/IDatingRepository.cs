d666e16beb7a64ef947be74321da36d81eeb7cfd
xchanmolx <cmanigos@gmail.com>
2020-02-01 11:28:28 +0800

    End of Section 14

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using DatingApp.API.Models;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API.Data
 {
@@ -9,7 +10,7 @@ namespace DatingApp.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
